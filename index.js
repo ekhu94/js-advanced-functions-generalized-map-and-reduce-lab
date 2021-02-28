7fb183d2e9897b061732a16710fd3f14c54e162e
Erik Huang <erikhuangdrums@gmail.com>
2021-02-28 07:34:34 -0800

    commit tree 269e8f3126be2da2d4e45efc5f4ed203f8a3fada

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+const map = (arr, func) => {
+    return arr.map(n => func(n));
+}
+
+const reduce = (arr, func, val=null) => {
+    let output = !!val ? val : arr[0];
+    for (let i = !!val ? 0 : 1; i < arr.length; i++) {
+        output = func(arr[i], output)
+    }
+    return output;
+}
